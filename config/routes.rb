927a0b889d0fdd92ac30e13fb4388604d4701b91
atp1o2 <andrewpham8@gmail.com>
2015-09-12 20:10:04 -0700

    setup database, controllers, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
+
+  controller :home do
+    get 'contacts' => 'home#contacts'
+  end
+
+  resources :projects
+
+  resources :blogs do
+    resources :comments
+  end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
